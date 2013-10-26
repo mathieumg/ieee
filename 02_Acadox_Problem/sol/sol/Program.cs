249e5918afe69365bd2507c49e7cd0e1cc67023a
Mathieu Parent <mathieu-2.parent@polymtl.ca>
2013-10-25 22:42:22 -0400

    v1 acadox (Manque 2 case)

---
+++ 02_Acadox_Problem/sol/sol/Program.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+public abstract class Operator
+{
+    public virtual int DoWork(int val1, int val2)
+    {
+        return FormatOverflowUnderflow(DoWorkActual(val1, val2));
+    }
+
+    public abstract int DoWorkActual(int val1, int val2);
+    public virtual int GetNumArgs()
+    {
+        return 2;
+    }
+
+    public int FormatOverflowUnderflow(int val)
+    {
+        if (val > 0xFFFF)
+        {
+            return 0xFFFF;
+        }
+        else if (val < 0x0000)
+        {
+            return 0x0000;
+        }
+        return val;
+    }
+}
+
+public class Plus : Operator
+{
+    public override int DoWorkActual(int val1, int val2)
+    {
+        return val1+val2;
+    }
+}
+
+public class Minus : Operator
+{
+    public override int DoWorkActual(int val1, int val2)
+    {
+        return val1-val2;
+    }
+}
+
+public class LogicalAnd : Operator
+{
+    public override int DoWorkActual(int val1, int val2)
+    {
+
+        return (val1&0xFFFF)&(val2&0xFFFF);
+    }
+}
+
+public class LogicalOr : Operator
+{
+    public override int DoWorkActual(int val1, int val2)
+    {
+        return (val1&0xFFFF)|(val2&0xFFFF);
+    }
+}
+
+public class LogicalNot : Operator
+{
+    public override int DoWorkActual(int val1, int val2)
+    {
+        return ~(val1&0xFFFF);
+    }
+
+    public override int GetNumArgs()
+    {
+        return 1;
+    }
+}
+
+public class LogicalExclusiveOr : Operator
+{
+    public override int DoWorkActual(int val1, int val2)
+    {
+        return (val1&0xFFFF)^(val2&0xFFFF);
+    }
+}
+
+
+class Solution
+{
+
+    static void PrintError()
+    {
+        System.Console.Out.WriteLine("ERROR");
+        System.Console.In.Read();
+        Environment.Exit(0);
+    }
+
+    static void PrintSolution(string val)
+    {
+        string outVal = val;
+        outVal = outVal.ToUpper();
+        outVal = outVal.PadLeft(4, '0');
+        System.Console.Out.WriteLine(outVal);
+        System.Console.In.Read();
+    }
+
+    static Operator GetOperator(string str)
+    {
+        switch (str)
+        {
+            case "+":
+                return new Plus();
+            case "-":
+                return new Minus();
+            case "&":
+                return new LogicalAnd();
+            case "|":
+                return new LogicalOr();
+            case "~":
+                return new LogicalNot();
+            case "X":
+                return new LogicalExclusiveOr();
+        }
+        return null;
+    }
+
+
+    static void Main(String[] args)
+    {
+        System.Collections.Stack stack = new System.Collections.Stack();
+        using (TextReader tr = System.Console.In)
+        {
+                try
+                {
+                    stack.Clear();
+                    string line=tr.ReadLine();
+                    line=line.Trim();
+                    line=line.ToUpper();
+                    string[] tokensStr=line.Split(' ');
+
+                    if (tokensStr.Length>20||line.Length == 0)
+                    {
+                        Solution.PrintError();
+                    }
+
+                    foreach (string str in tokensStr)
+                    {
+                        Operator op=Solution.GetOperator(str);
+
+                        if (op==null)
+                        {
+                            if (str.Length > 4)
+                            {
+                                Solution.PrintError();
+                            }
+                            stack.Push(str);
+                        }
+                        else
+                        {
+                            int nbOperandsNecessary=op.GetNumArgs();
+                            if (stack.Count<nbOperandsNecessary)
+                            {
+                                Solution.PrintError();
+                                break;
+                            }
+                            else
+                            {
+                                int[] vals;
+                                vals=new int[2];
+                                vals[0]=0;
+                                vals[1]=0;
+
+                                for (int i=0;i<nbOperandsNecessary;i++)
+                                {
+                                    try
+                                    {
+                                        string popedVal=stack.Pop().ToString();
+                                        vals[1-i]= (int) Convert.ToUInt32(popedVal, 16);
+                                    }
+                                    catch (Exception)
+                                    {
+                                        Solution.PrintError();
+                                        break;
+                                    }
+                                }
+
+                                int retVal=op.DoWork(vals[0], vals[1]);
+                                string retValStr=retVal.ToString("X");
+                                stack.Push(retValStr);
+                            }
+                        }
+                    }
+
+                    if (stack.Count!=1)
+                    {
+                        Solution.PrintError();
+                    }
+                    else
+                    {
+                        string lastVal=stack.Pop().ToString();
+                        try
+                        {
+                            Convert.ToUInt32(lastVal, 16);
+                        }
+                        catch (Exception)
+                        {
+                            Solution.PrintError();
+                        }
+                        Solution.PrintSolution(lastVal);
+                    }
+                }
+                catch (Exception)
+                {
+                    PrintError();
+                }
+        }
+    }
+}
